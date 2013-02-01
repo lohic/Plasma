f7a7ab85b86f6f6f4334cad0a6f6327de70d32b2
Loïc Horellou <contact@formidable-studio.net>
2013-02-01 11:55:10 +0100

    update variables de connexion

--- vars/constantes_vars.php
+++ vars/constantes_vars.php
@@ -3,10 +3,11 @@
 //echo getcwd().'/../';
 
 define ('SUB_FOLDER',			'Site_SCIENCESPO_PLASMA/');
-define ('ABSOLUTE_URL',			'http://127.0.0.1:8080/Mercenariat/2012-04-02 Syclo - ScPo Plasma/GitHub/plasma/'.SUB_FOLDER);
+define ('ABSOLUTE_URL',			'http://localhost:8888/'.SUB_FOLDER);
 define ('LOCAL_PATH',			getcwd().'/../');
 
 define ('IS_LDAP_SERVER',		false);
+define ('IS_MAIL_LOGIN',		true);
 
 define ('SLIDE_TEMPLATE_FOLDER','slides_templates/');
 define ('ACTU_MEDIA_FOLDER',	'actu_medias/');
