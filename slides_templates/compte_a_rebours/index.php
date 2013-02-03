ba797a0cf42c1947335f3994cbb9fa9cf79fbd1f
Loïc Horellou <contact@formidable-studio.net>
2013-02-03 19:02:30 +0100

    update compte à rebours

--- slides_templates/compte_a_rebours/index.php
+++ slides_templates/compte_a_rebours/index.php
@@ -1,15 +1,21 @@
-	<div class="colonne">
-		<div class="header">
-			<h1>J-14</h1>
-		</div>
-		
-		<div class="image">
-			<img src="" width="768" alt="image"/>
-		</div>
+<div id="ombre">
 
-		<div class="footer">
-			<div class="logo">
-				<img src="logo.png" alt="Sciences Po"/>
-			</div>
-		</div>
-	</div>
+</div>
+<div class="colonne">
+
+	<div class="edit textfield date invisible" id="A" title="A - Date de l'événement"></div>
+
+    <div class="header">
+        <h1></h1>
+    </div>
+    
+    <div class="visuel edit image" id="I" title="I - image">
+        <img src="" width="768" alt="image" class="image-event" />
+    </div>
+
+    <div class="footer">
+        <div class="logo">
+            <img src="logo.png" alt="Sciences Po" />
+        </div>
+    </div>
+</div>
--- slides_templates/compte_a_rebours/script.js
+++ slides_templates/compte_a_rebours/script.js
@@ -1,2 +1,27 @@
 
 // Javascript
+
+function decompte(){
+	
+	dateArray = $('#A').html().split('-');
+	
+	today = new Date();
+	dateEvent = new Date(dateArray[0], dateArray[1]-1, dateArray[2]);
+	
+	difference = today - dateEvent;
+	days = Math.round(difference/(1000*60*60*24))-1;
+	if(days>=0){ days = '+'+days; }
+	
+	// display
+	$('.header h1').first().html('J'+days);
+	
+	// boucle	
+	setTimeout(decompte, 60010);
+	
+}
+
+$(document).ready(function(){
+	
+	decompte();
+		
+});
