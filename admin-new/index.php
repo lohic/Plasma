ebf40fe9261fdebbd95f68cbb3bbb465bb3dbd4d
Loïc Horellou <loic@formidable-studio.net>
2013-08-26 19:01:12 +0200

    update admin-new/index.php
    ajout les librairies css + javascript
    + structure dom

--- admin-new/index.php
+++ admin-new/index.php
@@ -35,113 +35,129 @@ $ecran 		= new Ecran($id_plasma);
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>Sciences Po | Administration des écrans plasma</title>
-<link href="../css/admin.css" rel="stylesheet" type="text/css" />
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Sciences Po | Administration des écrans plasma</title>
+	<link href="../css/admin.css" rel="stylesheet" type="text/css" />
 
-<link type="text/css" href="../css/sciencespo-jquery-ui/jquery-ui-1.8.22.custom.css" rel="Stylesheet" />	
-<script type="text/javascript" src="../js/jquery-1.7.2.min.js"></script>
-<script type="text/javascript" src="../js/jquery-ui-1.8.22.custom.min.js"></script>
+	<link href="../css/sciencespo-jquery-ui/jquery-ui-1.8.22.custom.css" rel="stylesheet" type="text/css">
+    <link href="../css/uploadifive.css" rel="Stylesheet" type="text/css" />
+    <link href="../css/timeline.css" rel="stylesheet" type="text/css">
+    <link href="../css/sciencespo.jquery.fancybox.css" rel="stylesheet" type="text/css" media="screen" />
 
-<script type="text/javascript" src="../js/jquery-ui-timepicker-addon.js"></script>
-<script type="text/javascript" src="../js/jquery.iframe-post-form.js"></script>
-<script type="text/javascript" src="../js/jquery.dragsort-0.3.10.js"></script>
-<script type="text/javascript" src="../js/jquery.cookie.js"></script>
-<script type='text/javascript' src='../js/jquery.form.js'></script>
-<script type="text/javascript" src="../js/tiny_mce/tiny_mce.js"></script>
-<script type='text/javascript' src='../js/animatedcollapse.js'></script>
-<script type="text/javascript" src="../js/json-minified.js"></script>
-<script type="text/javascript" src="../js/splitter.js"></script>
-<script type="text/javascript" src="../js/fonctions.js"></script>
-
-<!-- SLiDES -->
-
-<link type="text/css" href="uploadifive/uploadifive.css" rel="Stylesheet" />	
-<script type="text/javascript" src="../js/jquery.uploadifive.min.js"></script>
-<!--<script type="text/javascript" src="../js/jquery.upload-1.0.2.min.js"></script>-->
-<!--<script language="javascript" type="text/javascript" src="../js/slide.setInputs.js"></script>
-<script language="javascript" type="text/javascript" src="../js/slide.contentParser.js"></script>-->
-<script language="javascript" type="text/javascript" src="../js/slide.compteur-field.js"></script>
-
-
-
-
-
-<script type="text/javascript">
-
-    <!--
-
-	//GESTION DE MENU
-
-	$(document).ready( function () {
-
-		$('ul#menuDown > li').mouseover(function(){ $(this).children('a').addClass('menuDown-hover').siblings('ul').show(); });
-		$('ul#menuDown > li').mouseout(function(){ $(this).children('a').removeClass('menuDown-hover').siblings('ul').hide(); });
-		
-
-		// GESTION DU MENU PRINCIPAL
-
-		$("#globalnav>ul>li>a").each( function () {	
-			$(this).attr("class","");
-		} ) ;
+	<!-- JQUERY -->
+	<script type="text/javascript" src="../js/jquery-1.10.2.min.js"></script>
+    <!-- ICANHAZ -->
+    <script type="text/javascript" src="../js/ICanHaz.min.js" ></script>
+    <!-- TIMLINE -->
+    <script src="../js/timeline-min.js" type="text/javascript"></script>
+    <script src="../js/timeline-locales.js" type="text/javascript"></script>
+	<!-- JQUERY UI -->
+	<script type="text/javascript" src="../js/jquery-ui-1.10.3.custom.min.js"></script>
+	<script type="text/javascript" src="../js/jquery-ui-timepicker-addon.js"></script>
+	<script type="text/javascript" src="../js/jquery.iframe-post-form.js"></script>
+	<script type="text/javascript" src="../js/jquery.dragsort-0.3.10.js"></script>
+	<script type="text/javascript" src="../js/jquery.cookie.js"></script>
+	<script type='text/javascript' src='../js/jquery.form.js'></script>
+	<!-- TINY MCE -->
+	<script type="text/javascript" src="../js/tinymce/tinymce.min.js"></script>
+	<script type='text/javascript' src='../js/animatedcollapse.js'></script>
+    <!-- FANCYBOX -->
+    <script type="text/javascript" src="../js/jquery.mousewheel-3.0.6.pack.js"></script>
+    <script type="text/javascript" src="../js/jquery.fancybox.js?v=2.1.4"></script>
+	<!--<script type="text/javascript" src="../js/json-minified.js"></script>-->
+	<!--<script type="text/javascript" src="../js/splitter.js"></script>-->
+	<script type="text/javascript" src="../js/fonctions.js"></script>
+	<!-- UPLOADIFIVE -->
+    <script type="text/javascript" src="../js/uploadifive-v1.1.2-standard/jquery.uploadifive.js"></script>
+	<!-- SLiDES -->
+	<!--<script type="text/javascript" src="../js/jquery.upload-1.0.2.min.js"></script>-->
+	<!--<script language="javascript" type="text/javascript" src="../js/slide.setInputs.js"></script>
+	<script language="javascript" type="text/javascript" src="../js/slide.contentParser.js"></script>-->
+	<script type="text/javascript" src="../js/slide.compteur-field.js"></script>
 
 
-		$("#globalnav>ul>li>a").click( function () {
-			$("#globalnav>ul>li>a").each( function () {
+
+	<script type="text/javascript" src="../js/script.js"></script>
+
+
+
+
+
+	<script type="text/javascript">
+
+	    <!--
+
+		//GESTION DE MENU
+
+		$(document).ready( function () {
+
+			$('ul#menuDown > li').mouseover(function(){ $(this).children('a').addClass('menuDown-hover').siblings('ul').show(); });
+			$('ul#menuDown > li').mouseout(function(){ $(this).children('a').removeClass('menuDown-hover').siblings('ul').hide(); });
+			
+
+			// GESTION DU MENU PRINCIPAL
+
+			$("#globalnav>ul>li>a").each( function () {	
 				$(this).attr("class","");
-
 			} ) ;
 
-			$(this).attr("class","select");	
-		} ) ;
 
+			$("#globalnav>ul>li>a").click( function () {
+				$("#globalnav>ul>li>a").each( function () {
+					$(this).attr("class","");
 
+				} ) ;
 
-		$("li span.trash").each( function () {	
-			$(this).click( function () {
-
-				$(this).parent().remove();
-				var order = '';
-
-				$('.news_list').each( function () {
-					order += $(this).attr('id') +':'+ $(this).sortable('toArray')+'|';
-				});
-
-				//alert(order);
-
-				var valeur = document.getElementById("save_value");
-				valeur.value = order;
-
-				$('#return_refresh').text('état : Sauvegarde en cours !');
-				$('#refresh_form').submit();
+				$(this).attr("class","select");	
 			} ) ;
-		} ) ;
-		
-		
-		// UPLOADIFIVE
-		$('input[name="file"]').uploadifive( {
-			'uploadScript' : 'uploadifive/uploadifive.php',
-			'removeCompleted' : true,
-			'fileSizeLimit' : 0,
-			'method' : 'post',
-			'formData' : { 'subfolder' : $('input[name="file"]').attr('subfolder') },
-			'onUploadComplete' : function(file, data) {
-				fieldname = $('input[name="file"]').attr('fieldname');
-				$('input[name="'+fieldname+'"]').val(data);
-				$('img[name="'+fieldname+'"]').attr('src', '../'+data);
-			}
-		} );
-	} ) ;
 
-	$(function() {
-		$(".date").datepicker({
-			dateFormat: 'yy-mm-dd',
-			firstDay: 1,
-			dayNamesMin : ['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'],
-			monthNames : ['Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre']
+
+
+			$("li span.trash").each( function () {	
+				$(this).click( function () {
+
+					$(this).parent().remove();
+					var order = '';
+
+					$('.news_list').each( function () {
+						order += $(this).attr('id') +':'+ $(this).sortable('toArray')+'|';
+					});
+
+					//alert(order);
+
+					var valeur = document.getElementById("save_value");
+					valeur.value = order;
+
+					$('#return_refresh').text('état : Sauvegarde en cours !');
+					$('#refresh_form').submit();
+				} ) ;
+			} ) ;
+			
+			
+			// UPLOADIFIVE
+			$('input[name="file"]').uploadifive( {
+				'uploadScript' : 'uploadifive/uploadifive.php',
+				'removeCompleted' : true,
+				'fileSizeLimit' : 0,
+				'method' : 'post',
+				'formData' : { 'subfolder' : $('input[name="file"]').attr('subfolder') },
+				'onUploadComplete' : function(file, data) {
+					fieldname = $('input[name="file"]').attr('fieldname');
+					$('input[name="'+fieldname+'"]').val(data);
+					$('img[name="'+fieldname+'"]').attr('src', '../'+data);
+				}
+			} );
+		} ) ;
+
+		$(function() {
+			$(".date").datepicker({
+				dateFormat: 'yy-mm-dd',
+				firstDay: 1,
+				dayNamesMin : ['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'],
+				monthNames : ['Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre']
+			});
 		});
-	});
-</script>
+	</script>
 
 
 
