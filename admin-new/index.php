f8d0b2c854debafe24d398183d0fe1869d959170
Loïc Horellou <loic@formidable-studio.net>
2013-08-30 16:28:08 +0200

    changement entete DOM
    -> html5

--- admin-new/index.php
+++ admin-new/index.php
@@ -31,10 +31,8 @@ $id_plasma	= !empty($_GET['id_plasma'])?$_GET['id_plasma']:NULL;
 $ecran 		= new Ecran($id_plasma);
 
 
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+?><html>
 <head>
-
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Sciences Po | Administration des écrans plasma</title>
 	<link href="../css/admin.css" rel="stylesheet" type="text/css" />
