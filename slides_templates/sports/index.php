085df0d9641db98bb983307c312222fd6930c268
Loïc Horellou <loic@formidable-studio.net>
2013-10-03 08:48:40 +0200

    update template sports

--- slides_templates/sports/index.php
+++ slides_templates/sports/index.php
@@ -7,28 +7,30 @@
 		</div>
 		
 		<div class="sport">
-			<h2><span class="edit textfield" max="30" id="B" title="B - Catégorie de sport">{{type_sport}}</span></h2>
+			<h2>{{type_sport}}</h2>
 			{{#icone_sport}}
 			<div class="le_sport" style="background-image:url(<?php echo ABSOLUTE_URL.SLIDE_TEMPLATE_FOLDER.'sports/pictos/'; ?>{{icone_sport}})"></div>
 			{{/icone_sport}}
 		</div>
 
 		<div class="texte">
-			<h3><span class="edit textfield" max="44" id="D" title="D - Nom du championnat ou tournoi">{{titre}}</span></h3>
+			<h3>{{titre}}</h3>
 		</div>
 
 		<div class="texte2">
-			<h3><span class="edit textfield" max="25" id="E" title="E - Nom de l'équipe ou du sportif">{{participant}}</span></h3>
+			<h3>{{participant}}</h3>
 		</div>
+
+		<div class="endbloc"></div>
 	</div>
 
 
 	<div class="colonne_droite">
 		<div class="classement">
-			<h3><span class="edit textfield" max="30" id="F" title="F - Type de résultat">{{type_resultat}}</span></h3>
+			<h3>{{type_resultat}}</h3>
 		</div>
 
 		<div class="detail_classement">
-			<span class="edit textarea" id="G" title="G - Résultat(s)">{{{resultat}}}</span>
+			{{{resultat}}}
 		</div>
 	</div>
