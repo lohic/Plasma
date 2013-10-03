72ec4981d0f08a7e88d9362f822945367a92e7b5
Loïc Horellou <loic@formidable-studio.net>
2013-10-03 08:48:48 +0200

    update template verbatim

--- slides_templates/verbatim/index.php
+++ slides_templates/verbatim/index.php
@@ -1,11 +1,11 @@
 <div id="ombre"></div>
-<div class="header" id="A" title="A - titre du template verbatim  :)" max="22">
+<div class="header">
 	<h1>Le Saviez-vous?</h1>
 </div>
-<div class="texte edit textarea" id="B" title="B - texte" max="11">
+<div class="texte">
 	{{{verbatim}}}
 </div>
-<div class="footer" id="C" title="C - Footer">
+<div class="footer">
 	<img src="<?php echo ABSOLUTE_URL.SLIDE_TEMPLATE_FOLDER.'verbatim/'; ?>logo.png" alt="Sciences Po"/>
 </div>
 
