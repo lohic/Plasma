affc07a3b6ed17387828f4123c1df8c66dde9a95
Loïc Horellou <loic@formidable-studio.net>
2013-10-03 08:49:01 +0200

    update template vidéo

--- slides_templates/video/index.php
+++ slides_templates/video/index.php
@@ -1,31 +1,25 @@
-<div id="ombre">
+<div id="ombre"></div>
 
-	</div>
 <div class="colonne">
 	<div class="header">
-		<h2>
-			<span class="edit textfield" max="22" id="A" title="A - Type de vidéo">{{type}}</span>
-		</h2>
+		<h2>{{type}}</h2>
 	</div>
 	
 	<div class="texte">
-		<h1>"
-			<span class="edit textfield" max="34" id="B" title="B - Titre/thème">{{titre}}</span>
-		"</h1>
+		<h1>« {{titre}} »</h1>
 	</div>
 
 	<div class="texte2">
 		<h2>
-			<span class="edit textfield" max="34" id="C" title="C - Nom de l'intervenant">{{intervenant}}</span>,<br/>
-			<span class="small edit textfield" max="39" id="D" title="D - Qualité de l'intervenant">{{qualite}}</span>
+			{{intervenant}},<br/>
+			<span class="small" title="Qualité de l'intervenant">{{qualite}}</span>
 		</h2>
 	</div>
 
-	<span class="edit video" id="E" title="E - Vidéo">
-		<video id="video" autoplay="true" src="<?php echo ABSOLUTE_URL.IMG_SLIDES; ?>{{fichier_video}}">
-			<p>{{description}}</p>
-		</video>
-	</span>
+	<video id="video" autoplay="true" src="<?php echo ABSOLUTE_URL.IMG_SLIDES; ?>{{fichier_video}}">
+		<p>{{description}}</p>
+	</video>
+		
 	<div class="footer">
 		<div class="logo">
 			<img src="<?php echo ABSOLUTE_URL.SLIDE_TEMPLATE_FOLDER.'video/'; ?>logo.png" alt="Sciences Po"/>
--- slides_templates/video/script.js
+++ slides_templates/video/script.js
@@ -10,16 +10,9 @@ $(document).ready(function(){
 
 function loadMovie(){
 	
-	//var videoURL = '../'+$('.video').html().replace(/\s/g, '').replace(/(\r\n|\n|\r)/gm,"");
-	//alert('#'+videoURL+'#');
-		
-	//$('.video').html('<video id="video" autoplay="true" src="'+videoURL+'"><p>La vidéo ne fonctionne pas</p></video>');
-	
 	$("video")
 	.bind("ended", function(e) {
 		e.preventDefault();
-		// sortie
-		//exit_slideshow();
 	})
 	.bind("loadedmetadata", function(e){
 		e.preventDefault();
@@ -27,9 +20,6 @@ function loadMovie(){
         var height = this.videoHeight;
         var duration = this.duration; // durée en ms
 		
-		// on stoppe le compte à rebour...
-		//clear_slideshow();
-		//
 		console.log(width+" "+height+" "+duration);
 	});
 }
