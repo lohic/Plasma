c7950c2b1fe441a8070f1bb9cca28436d45e64e4
Loïc Horellou <loic@formidable-studio.net>
2013-10-03 08:48:05 +0200

    update template default

--- slides_templates/default/index.php
+++ slides_templates/default/index.php
@@ -1,15 +1,15 @@
 <!-- SLIDE DEFAULT -->
 <div id="ombre"></div>
 
-<div class="header" id="A">
+<div class="header">
 	<h1>Écran {{titre_ecran}}</h1>
 </div>
 
-<div class="texte" id="B">
+<div class="texte">
 	<p>Aucune information à diffuser.<p>
 </div>
 
-<div class="footer" id="C">
+<div class="footer">
 	{{# logo }}
 	<img src="<?php echo ABSOLUTE_URL.SLIDE_TEMPLATE_FOLDER.'default/'; ?>logo.png" alt="Sciences Po"/>
 	{{/ logo }}
