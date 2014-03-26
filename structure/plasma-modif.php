ecf8da0966aef0fbfd4216cbe2cfa501dad8723e
Loïc Horellou <loic@formidable-studio.net>
2014-03-26 17:16:00 +0100

    nettoyage formulaire création d'écran

--- structure/plasma-modif.php
+++ structure/plasma-modif.php
@@ -34,7 +34,7 @@ if(empty($data->id_groupe)){
 <div class="form_container">
 	<p class="intro_modif"><?php echo isset($id_plasma) ? 'Modification' : 'Création'; ?> de l'écran : <a href="../slideshow/?plasma_id=<?php echo $data->id; ?>&preview&debug" target="_blank"><img src="../graphisme/eye.png" alt="voir"/></a></p>
 	<h3><?php echo $data->nom; ?></h3>
-	<div class="options"> <a href="?page=ecrans_modif&id_plasma=<?php echo $data->id; ?>&publish=ecran"> Publier l'écran </a> </div>
+	<!--<div class="options"> <a href="?page=ecrans_modif&id_plasma=<?php echo $data->id; ?>&publish=ecran"> Publier l'écran </a> </div>-->
 	<form action="" method="post" id="modif_ecran_info_form">
 		<input type="hidden" name="<?php echo isset($id_plasma)?'update':'create';?>" value="ecran"/>
 		
@@ -54,68 +54,10 @@ if(empty($data->id_groupe)){
 				<label for="id_groupe">écran relié au <a href="?page=ecrans_groupe_modif&id_groupe=<?php echo  $id_groupe;?>">groupe</a> :</label>
 				<?php echo func::createSelect($ecran->get_ecran_groupe_list(), 'id_groupe', $id_groupe, "onchange=\"$('#news_select_form').submit();\"", false ); ?> </p>
 		</fieldset>
-		<!--<fieldset>
-			<p>
-				<label for="id_playlist_locale">playlist locale :</label>
-				<?php echo func::createSelect($ecran->get_playlist_list(), 'id_playlist_locale', $data->id_playlist_locale, "", true ); ?>
-			</p>
-			<p>
-				<label for="id_playlist_nationale">playlist nationale :</label>
-				<?php echo func::createSelect($ecran->get_playlist_list(), 'id_playlist_nationale', $data->id_playlist_nationale, "", true ); ?>
-			</p>
-				
-		</fieldset>-->
+		
 		<input type="submit" name="edit_user" class="buttonenregistrer" id="edit_user" value="<?php echo isset($id_plasma) ? 'Modifier' : 'Créer'; ?>" />
 	</form>
 	
-	<?php if( !empty($data->id)){ ?>
 	
-	<div id="return_refresh"></div>
-	<form action="XMLrequest_update_slide_rel.php" method="post" id="modif_slide_list_form">
-		<input type="hidden" name="id_target" value="<?php echo $data->id; ?>" />
-		<input type="hidden" name="type_target" value="ecran" />
-		<fieldset>
-			<p class="legend"> <a href="javascript:" id="add_alerte_locale"> <img src="../graphisme/round_plus.png" alt="ajouter un slide" height="16"/> </a> ajouter une alerte locale </p>
-			<ul id="addslidealertelocale">
-				<?php echo $ecran->get_slide_alerte_list('ecran',75000); ?>
-			</ul>
-		</fieldset>
-		
-		<?php if ($core->userLevel <=1 ){ ?>
-		
-		<fieldset>
-			<p class="legend"> <a href="javascript:" id="add_alerte_nationale"> <img src="../graphisme/round_plus.png" alt="ajouter un slide" height="16"/> </a> ajouter une alerte nationale </p>
-			<ul id="addslidealertnationale">
-				<?php echo $ecran->get_slide_alerte_list('ecran','all',NULL); ?>
-			</ul>
-		</fieldset>
-		
-		<?php } ?>
-		<input type="hidden" value="" name="suppr_id_rel_slide" id="suppr_id_rel_slide">
-	</form>
-	<div class="reset"></div>
 </div>
 
-
-
-<div id="slideselector">
-	<div id="fleche"></div>
-	<div id="liste">
-		<div id="slide_select">
-			<form id="slide_select_form" action="XMLrequest_get_slide_list.php" method="get">
-				<input type="hidden" name="page" value="slides_select" />
-				<?php echo func::createSelect($slide->get_slide_template_list($core->groups_id)	, 'id_template'	, $id_template	, "onchange=\"$('#slide_select_form').submit();\"",true);?>
-				<?php echo func::createSelect($anneeListe, 'annee', $annee, "onchange=\"$('#slide_select_form').submit();\"", false ); ?>
-				<?php echo func::createSelect($moisListe, 'mois', $mois, "onchange=\"$('#slide_select_form').submit();\"", false ); ?>
-			</form>
-		</div>
-		<div id="id-selected-ecran"></div>
-		<div id="slidelisting">
-		</div>
-
-	</div>
-</div>
-
-<?php include_once('../structure/javascript-add-slide.php'); ?>
-
-<?php } ?>
