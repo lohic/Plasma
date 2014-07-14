ce81b8c42395bb75e1e966d27b1c3decd63b3873
Loïc Horellou <loic@formidable-studio.net>
2014-07-14 19:04:02 +0200

    upload slide PLANNING

---
+++ slides_templates/planning/index.php
@@ -0,0 +1,75 @@
+<div id="ombre1" class="ombre"></div>
+<div id="ombre2" class="ombre"></div>
+
+<div class="colonne_gauche">
+	<div class="header">
+		<h1>{{date_planning}}</h1>
+	</div>
+
+	<div class="logo" alt="Logo Sciences Po.">
+        <img src="<?php echo ABSOLUTE_URL.SLIDE_TEMPLATE_FOLDER.'planning/'; ?>logo.png" alt="Sciences Po"/>
+    </div>
+</div>
+
+<div class="footer">
+
+	<div class="planning" id="p1">
+		<div class="nom_salle">
+			<p>{{salle1}}</p>
+			<p>{{horaire1}}</p>	
+		</div>
+		<div class="descriptif">
+			<p>{{cours1}}</p>	
+		</div>
+	</div>
+
+	<div class="planning">
+		<div class="nom_salle">
+			<p>{{salle2}}</p>
+			<p>{{horaire2}}</p>		
+		</div>
+		<div class="descriptif">
+			<p>{{cours2}}</p>	
+		</div>
+	</div>
+
+	<div class="planning">
+		<div class="nom_salle">
+			<p>{{salle3}}</p>
+			<p>{{horaire3}}</p>	
+		</div>
+		<div class="descriptif">
+			<p>{{cours3}}</p>	
+		</div>
+	</div>
+
+	<div class="planning">
+		<div class="nom_salle">
+			<p>{{salle4}}</p>
+			<p>{{horaire4}}</p>		
+		</div>
+		<div class="descriptif">
+			<p>{{cours4}}</p>	
+		</div>
+	</div>
+
+	<div class="planning">
+		<div class="nom_salle">
+			<p>{{salle5}}</p>
+			<p>{{horaire5}}</p>		
+		</div>
+		<div class="descriptif">
+			<p>{{cours5}}</p>	
+		</div>
+	</div>
+
+	<div class="planning">
+		<div class="nom_salle">
+			<p>{{salle6}}</p>
+			<p>{{horaire6}}</p>		
+		</div>
+		<div class="descriptif">
+			<p>{{cours6}}</p>	
+		</div>
+	</div>
+</div>
---
+++ slides_templates/planning/script.js
@@ -0,0 +1,13 @@
+// Javascript
+// SLIDE PLANNING
+
+
+$(document).ready(function(){
+
+
+	$('body, th, td, .header, .texte, .footer, .texte2, #template').removeAttr( 'style' );
+
+	
+});
+
+
